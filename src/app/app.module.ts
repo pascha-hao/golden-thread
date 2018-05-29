7610bddfd9fcf655258184f1b16f9a4886dc9f58
Pascha Hao <pascha.hao@berkeley.edu>
2018-05-29 21:41:48 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,33 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { RegistrationPage } from '../pages/registration/registration';
+import { ProfilePage } from '../pages/profile/profile';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    RegistrationPage,
+    ProfilePage
+    
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
+    IonicModule.forRoot(MyApp) 
+],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    RegistrationPage,
+    ProfilePage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoginPage } from '../login/login';
+import { RegistrationPage } from '../registration/registration';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +11,14 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  navigateToLogin() {
+    this.navCtrl.push(LoginPage)
+
+  }
+  navigateToRegistration(){
+    this.navCtrl.push(RegistrationPage)
   }
 
 }
